d213cb4e60b4519268b5c6323116d108929cd567
Devansh Arya <devansh.arya900@gmail.com>
2026-07-21 13:58:41 +0530

    Model

--- rag-service/generator.py
+++ rag-service/generator.py
@@ -7,7 +7,7 @@ load_dotenv()
 
 API_KEY = os.getenv("OPENROUTER_API_KEY")
 
-MODEL = "poolside/laguna-m.1:free"
+MODEL = "nvidia/nemotron-3-ultra-550b-a55b:free"
 
 
 def generate_analysis(movie_name, context):
